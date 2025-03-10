7f8ed2bc3c65ae4eeb982d8624a6258dfa696cea
eCrMarques <126702522+eCrMarques@users.noreply.github.com>
2025-03-10 00:13:54 +0000

    Desafio: nível aventureiro Movimentando o Cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    int torre_passos = 5, bispo_passos = 5, rainha_passos = 8;
+    int torre_passos = 5;
+    int bispo_passos = 5;
+    int rainha_passos = 8;
+    int cavalo_passos = 5;
 
+    // Torre
     printf("\nTorre %d Passos \n", torre_passos);
     for (int i = 0 ; i < torre_passos ; i++){
         printf("Direita\n");
     }
     
+
+    // Bispo
     printf("\nBispo %d Passos \n", bispo_passos);
     for (int i = 1 ; i <= bispo_passos*2 ; i++){
         if (i % 2 == 0){
@@ -17,18 +23,29 @@ int main(){
         }
     }
     
+    // Rainha
     printf("\nRainha %d Passos \n", rainha_passos);
     for (int i = 0 ; i < rainha_passos ; i++){
         printf("Esquerda\n");
+    }
+
+    // Cavalo
+    printf("\nCavalo %d Passos \n", cavalo_passos);
+    for (int i = 1; i <= cavalo_passos ; i++){
+        int j = 1;
+        do{
+            if (j % 3 == 0 ){
+                printf("Direita\n");
+            }else{
+                printf("Cima-");
+            }
+            j++;
+        }while(j <= 3);
     }
 
     return 0;
 
 }
 
-
-// Torre: Move-se em linha reta horizontalmente ou verticalmente. Seu programa deverá simular o movimento da Torre cinco casas para a direita.
-// Bispo: Move-se na diagonal. Seu programa deverá simular o movimento do Bispo cinco casas na diagonal para cima e à direita. Para representar a diagonal, você imprimirá a combinação de duas direções a cada casa (ex: "Cima, Direita").
-// Rainha: Move-se em todas as direções. Seu programa deverá simular o movimento da Rainha oito casas para a esquerda.
-
-// O programa deverá imprimir no console a direção do movimento a cada casa percorrida pela peça. Para movimentos na diagonal (Bispo), imprimir a combinação de duas direções. Utilize o comando printf para exibir as informações. As saídas devem seguir o padrão: printf("Cima\n");, printf("Baixo\n");, printf("Esquerda\n");, printf("Direita\n");, printf("Cima Esquerda\n"); printf("Direita\n");
+// Lógica de Movimentação: O programa deverá implementar a lógica específica do movimento em "L" do Cavalo usando loops aninhados (um loop for e um loop while ou do-while).
+// Saída de Dados: O programa deverá imprimir no console a direção de cada etapa do movimento do Cavalo ("Baixo", "Baixo", "Esquerda"), utilizando o comando printf. Para as outras peças as saídas devem seguir o padrão: printf("Cima\n");, printf("Baixo\n");, printf("Esquerda\n");, printf("Direita\n");. Separe o movimento do Cavalo dos movimentos anteriores com uma linha em branco.
